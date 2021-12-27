5915597403b0def2898799667b0df8185f916fba
NileshJesani <47317458+NileshJesani@users.noreply.github.com>
2021-12-27 17:42:30 +0530

    Update FurnitureOrder.java
    Added some of the todo items, it is in progress

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -7,20 +7,25 @@ public class FurnitureOrder implements FurnitureOrderInterface {
      * TODO: Create a map of Furniture items to order quantities
      */
 
+    HashMap<Furniture, Integer> furItemOrdQuan;
+    
+    
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
+        furItemOrdQuan = new HashMap();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
-        // TODO: Complete the method
+        furItemOrdQuan.put(type, furnitureCount);
+        
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return furItemOrdQuan;
     }
 
     public float getTotalOrderCost() {
